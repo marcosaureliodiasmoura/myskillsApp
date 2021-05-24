61ab3f3b00469386be70fc52e4a8a197f4c76909
marcosaureliodiasmoura <marcos_moura@live.com>
2021-05-24 20:38:49 -0300

    StatusBar

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Home } from './src/pages/Home';
+import { StatusBar } from 'react-native';
 
 export default function App() {
-  return <Home />
+  return (
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" />
+    </>
+  )
+
 }
